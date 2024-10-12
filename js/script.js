717d3334077595e634faa799d7f1c19055f3e330
kowsar14238 <kowsar15-14238@diu.edu.bd>
2024-10-12 12:51:55 +0600

    Responsive home desgine

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ $(".slider-responsive").slick({
   infinite: true,
   speed: 300,
   slidesToShow: 10,
+  autoplay: true,
+  autoplaySpeed: 2000,
   slidesToScroll: 1,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1025,
       settings: {
         slidesToShow: 6,
         slidesToScroll: 1,
@@ -14,22 +16,21 @@ $(".slider-responsive").slick({
       },
     },
     {
-      breakpoint: 600,
+      breakpoint: 769,
       settings: {
-        slidesToShow: 2,
+        arrows: false,
+        slidesToShow: 4,
         slidesToScroll: 2,
       },
     },
     {
-      breakpoint: 480,
+      breakpoint: 357,
       settings: {
-        slidesToShow: 1,
+        arrows: false,
+        slidesToShow: 2,
         slidesToScroll: 1,
       },
     },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ],
 });
 $(".slider-responsive2").slick({
@@ -37,32 +38,33 @@ $(".slider-responsive2").slick({
   infinite: true,
   speed: 300,
   slidesToShow: 6,
+  autoplay: true,
+  autoplaySpeed: 2000,
   slidesToScroll: 1,
   responsive: [
     {
-      breakpoint: 1024,
+      breakpoint: 1025,
       settings: {
-        slidesToShow: 3,
+        slidesToShow: 4,
         slidesToScroll: 1,
         infinite: true,
       },
     },
     {
-      breakpoint: 600,
+      breakpoint: 769,
       settings: {
-        slidesToShow: 2,
+        arrows: false,
+        slidesToShow: 3,
         slidesToScroll: 2,
       },
     },
     {
-      breakpoint: 480,
+      breakpoint: 357,
       settings: {
-        slidesToShow: 1,
+        arrows: false,
+        slidesToShow: 2,
         slidesToScroll: 1,
       },
     },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ],
 });
